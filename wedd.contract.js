64ba2fc6d70280c95a0c0194742f8e93de16c381
evg merk <75069089+evsevy@users.noreply.github.com>
2022-08-08 19:15:03 +0300

    wedding smart-contract.js
    a small example of wed/contract on blockchaine

---
+++ wedd.contract.js
@@ -0,0 +1,259 @@
+contract Wedding is IWedding, WeddingEventEmitter {
+  IWeddingManager public weddingManager;
+  address public partner1;
+  address public partner2;
+  string public p1Name;
+  string public p2Name;
+  string public p1Vows;
+  string public p2Vows;
+  string public weddingPhoto;
+  bool public p1Answer;
+  bool public p2Answer;
+  bool public married;
+  bool public shouldHideGiftEvents;
+  uint256 public dateMarried;
+  uint256 public minGiftAmount;
+  mapping(address => bool) public banned;
+  Stage public stage;
+
+  enum Stage {
+    Uninitialized,
+    Initialized,
+    InProgress,
+    Married,
+    Divorced
+  }
+
+  modifier atStage(
+    Stage _stage
+  ) {
+    require(stage == _stage);
+
+    _;
+  }
+
+  modifier atEitherStage(
+    Stage _stage1,
+    Stage _stage2
+  ) {
+    require(
+      stage == _stage1 ||
+      stage == _stage2
+    );
+
+    _;
+  }
+
+  modifier onlyFiance() {
+    require(
+      msg.sender == partner1 ||
+      msg.sender == partner2
+    );
+
+    _;
+  }
+
+  function isContract(
+    address _address
+  )
+    internal
+    view
+    returns (bool)
+  {
+    uint256 _codeSize;
+
+    assembly {
+      _codeSize := extcodesize(_address)
+    }
+
+    return _codeSize > 0;
+  }
+
+  function initialize(
+    address _p1Address,
+    string calldata _name1,
+    address _p2Address,
+    string calldata _name2
+  )
+    external
+    atStage(Stage.Uninitialized)
+  {
+    require(isContract(msg.sender));
+    require(_p1Address != address(0));
+    require(_p2Address != address(0));
+    require(_p1Address != _p2Address);
+    require(bytes(_name1).length > 0);
+    require(bytes(_name2).length > 0);
+
+    weddingManager = IWeddingManager(msg.sender);
+    minGiftAmount = 1e16;
+    partner1 = _p1Address;
+    p1Name = _name1;
+    partner2 = _p2Address;
+    p2Name = _name2;
+    stage = Stage.Initialized;
+  }
+
+  function updateVows(
+    string calldata _vows
+  )
+    external
+    onlyFiance
+    atEitherStage(Stage.Initialized, Stage.InProgress)
+  {
+    msg.sender == partner1
+      ? p1Vows = _vows
+      : p2Vows = _vows;
+
+    emitVowsUpdated(address(weddingManager), msg.sender, _vows);
+  }
+
+  function acceptProposal()
+    external
+    onlyFiance
+    atEitherStage(Stage.Initialized, Stage.InProgress)
+  {
+    msg.sender == partner1
+      ? p1Answer = true
+      : p2Answer = true;
+
+    emitPartnerAccepts(address(weddingManager), msg.sender);
+
+    if (p1Answer && p2Answer) {
+      married = true;
+      dateMarried = block.timestamp;
+      stage = Stage.Married;
+
+      emitMarried(address(weddingManager), partner1, partner2);
+
+      return;
+    }
+
+    stage = Stage.InProgress;
+  }
+
+  function rejectProposal()
+    external
+    onlyFiance
+    atEitherStage(Stage.Initialized, Stage.InProgress)
+  {
+    emitWeddingCancelled(address(weddingManager), msg.sender);
+    weddingManager.removeCancelledWedding(partner1, partner2);
+
+    selfdestruct(msg.sender);
+  }
+
+  function updateWeddingPhoto(
+    string calldata _uri
+  )
+    external
+    onlyFiance
+    atStage(Stage.Married)
+  {
+    require(
+      keccak256(bytes(_uri)) !=
+      keccak256(bytes(weddingPhoto))
+    );
+    weddingPhoto = _uri;
+
+    emitWeddingPhotoUpdated(address(weddingManager), _uri);
+  }
+
+  function divorce()
+    external
+    onlyFiance
+    atStage(Stage.Married)
+  {
+    msg.sender == partner1
+      ? p1Answer = false
+      : p2Answer = false;
+
+    emitPartnerDivorces(address(weddingManager), msg.sender);
+
+    if (!p1Answer && !p2Answer) {
+      weddingManager.divorce(partner1, partner2);
+
+      selfdestruct(msg.sender);
+    }
+  }
+
+  function claimWeddingGifts()
+    external
+    onlyFiance
+  {
+    uint256 _contractBalance = address(this).balance;
+    require(_contractBalance > 0);
+
+    msg.sender.transfer(_contractBalance);
+
+    emitGiftClaimed(address(weddingManager), msg.sender, _contractBalance);
+  }
+
+  function sendWeddingGift(
+    string memory _message
+  )
+    public
+    payable
+  {
+    require(msg.value >= minGiftAmount);
+
+    emitGiftReceived(
+      address(weddingManager),
+      msg.sender,
+      msg.value,
+      _message
+    );
+  }
+
+  function deRegisterWedding()
+    external
+    onlyFiance
+  {
+    weddingManager.deRegisterWedding();
+  }
+
+  function updateUserPermissions(
+    address _user,
+    bool _banned
+  )
+    external
+    onlyFiance
+  {
+    require(banned[_user] != _banned);
+
+    banned[_user] = _banned;
+
+    emitUserPermissionUpdated(address(weddingManager), _user, _banned);
+  }
+
+  function updateMinGiftAmount(
+    uint256 _minGiftAmount
+  )
+    external
+    onlyFiance
+  {
+    minGiftAmount = _minGiftAmount;
+
+    emitMinGiftAmountUpdated(address(weddingManager), _minGiftAmount);
+  }
+
+  function updateShouldHideGiftEvents(
+    bool _shouldHideGiftEvents
+  )
+    external
+    onlyFiance
+  {
+    require(_shouldHideGiftEvents != shouldHideGiftEvents);
+
+    shouldHideGiftEvents = _shouldHideGiftEvents;
+
+    emitShouldHideGiftEventsUpdated(address(weddingManager), _shouldHideGiftEvents);
+  }
+
+  function()
+    external
+    payable
+  {
+    sendWeddingGift("");
+  }
+}
